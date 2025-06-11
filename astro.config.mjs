8772eeeece61f735b2b7e23a521e4c9e7ce67037
Keith Wine <keithmwine@gmail.com>
2025-06-10 21:27:04 -0400

    Add dark theme.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://earnest-bienenstitch-6dd1bd.netlify.app"
+  site: "https://earnest-bienenstitch-6dd1bd.netlify.app",
+  integrations: [preact()]
 });
